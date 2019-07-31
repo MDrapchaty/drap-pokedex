a99776ce41e1f888f2a3206763842983134b3ddd
Matt Drapchaty <Drapchaty@comcast.net>
2019-07-30 18:42:08 -0600

    added app class/styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
-        hellohello
       </div>
     );
   }
