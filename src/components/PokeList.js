e8e4c6e42ebce3d9a4094d5fef77c1d84deb0cbb
Matt Drapchaty <Drapchaty@comcast.net>
2019-07-30 18:46:29 -0600

    created pokelist and style

---
+++ src/components/PokeList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/PokeList.css';
+
+const PokeList = () => {
+  return (
+    <section className="poke-list">
+
+    </section>
+  )
+}
+
+
+export default PokeList;
