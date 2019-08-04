4836bf2a3d49ff953afd83863135f642f6e56a52
Matt Drapchaty <Drapchaty@comcast.net>
2019-08-04 05:21:03 -0600

    types properly showing

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -3,7 +3,7 @@ class Pokemon {
     this.id = data.id;
     this.name = data.name;
     this.sprite = data.sprites.front_default;
-    this.type =   data.types[0].type.name + ', ' + data.types[1].type.name;
+    this.type = (data.types.length) > 1 ? data.types[0].type.name + ', ' + data.types[1].type.name : data.types[0].type.name;
     this.height = ((''+data.height).length) > 1 ? (''+data.height)[0] + '.' + (''+data.height)[1] + ' m' : '.' + data.height + ' m';
     this.weight = (''+data.weight)[0] + '.' + (''+data.weight)[1] + ' kg'; 
   }
