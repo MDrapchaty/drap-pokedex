875afdc2b93e07047759aa5b0ee7516d37822782
Matt Drapchaty <Drapchaty@comcast.net>
2019-08-08 02:44:57 -0600

    fixed weight issue

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -5,9 +5,9 @@ class Pokemon {
     this.sprite = data.sprites.front_default;
     this.type1 = data.types[0].type.name;
     this.type2 = (data.types.length) > 1 ?  data.types[1].type.name : 'blank';
-    this.ability = 'Abilities: ' + data.abilities[0].ability.name.charAt(0).toUpperCase() + data.abilities[0].ability.name.slice(1) + ' / ' + data.abilities[1].ability.name.charAt(0).toUpperCase() + data.abilities[1].ability.name.slice(1);
+    this.ability = data.abilities.length > 1 ?  data.abilities[0].ability.name.charAt(0).toUpperCase() + data.abilities[0].ability.name.slice(1) + ' / ' + data.abilities[1].ability.name.charAt(0).toUpperCase() + data.abilities[1].ability.name.slice(1) : data.abilities[0].ability.name.charAt(0).toUpperCase() + data.abilities[0].ability.name.slice(1);
     this.height = ((''+data.height).length) > 1 ? 'Height: ' + (''+data.height)[0] + '.' + (''+data.height)[1] + ' m' : 'Height: .' + data.height + ' m';
-    this.weight = 'Weight: ' + (''+data.weight)[0] + '.' + (''+data.weight)[1] + ' kg'; 
+    this.weight = ((''+data.weight).length) > 1 ? 'Weight: ' + (''+data.weight).substring(0, (''+data.weight).length - 1) + '.' + (''+data.weight).substr(-1) + ' kg' : '.' + (''+data.weight)[0] + ' kg'; 
 
   }
     
